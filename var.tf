a5b752478ca4893402615f14e7011b356c80f5f0
sara <sara@gmail.com>
2025-04-17 15:14:01 +0530

    fisrt commit

---
+++ var.tf
@@ -0,0 +1,23 @@
+variable "AWS_REGION" {
+    default = "ap-southeast-1"
+}
+variable "AWS_AMI" {
+    default = "ami-0b5a4445ada4a59b1"
+}
+variable "az" {
+    default = "ap-southeast-1a"
+}
+variable "akey" {
+    default = ""
+}
+variable "skey" {
+    default = ""
+}
+variable "pkey" {
+    default = "Linux-Singapoore-2025"
+}
+variable "rpass" {
+    default = "111"
+}
+
+
